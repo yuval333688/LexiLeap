4c4902f9d69bb1608b558540382d131dd3398c61
yuval333688 <yuvalgefen11@gmail.com>
2025-05-26 11:52:53 +0300

    gyug

--- src/backEnd/server.py
+++ src/backEnd/server.py
@@ -1,16 +1,23 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS
+import OpenSSL
 
-app = Flask(__name__, static_folder='../static') # שנה את הנתיב בהתאם למיקום ה-static שלך
+
+app = Flask(__name__) # שנה את הנתיב בהתאם למיקום ה-static שלך
 CORS(app)
 
-@app.route('/', methods=['GET'])
+@app.route("/")
 def say_hi():
     return "hi"
 
-@app.route('/test_page') # מסלול חדש לקובץ ה-HTML
-def serve_test_page():
-    return send_from_directory(app.static_folder, 'simple_test.html')
+@app.route("/levels/get/<int:num>")
+def get_level(num):
+    str = ""
+    for i in range(0,num):
+        str +="a"
+    return str
+
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    ssl_context = ('certs/localhost+2.pem', 'certs/localhost+2-key.pem')
+    app.run(host='127.0.0.1', port=5000, ssl_context=ssl_context, debug=True)
